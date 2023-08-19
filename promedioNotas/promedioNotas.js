4c6b84d814291cdfdde19babd918e4a5b4b402fe
Andres Villacres <andreslkf14@gmail.com>
2023-08-19 18:07:21 -0500

    Gif aprovado o fracaso

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -1,8 +1,14 @@
 calcularPromedioNotas = function(){
-    let n1 = parseFloat(document.getElementById('txtNota1').value)
-    let n2 = parseFloat(document.getElementById('txtNota2').value)
-    let n3 = parseFloat(document.getElementById('txtNota3').value)
+    let n1 = parseFloat(document.getElementById('txtNota1').value);
+    let n2 = parseFloat(document.getElementById('txtNota2').value);
+    let n3 = parseFloat(document.getElementById('txtNota3').value);
 
-    let promedio = calcularPromedio(n1,n2,n3)
-    mostrarTexto('lblPromedio',promedio)
+    let promedio = calcularPromedio(n1,n2,n3);
+    promedio = promedio.toFixed(2)
+    mostrarTexto('lblPromedio',promedio);
+    if (promedio > 7) {
+        mostrarImagen('imgPromedio','imagenes/200w.gif');
+    } else {
+        mostrarImagen('imgPromedio','imagenes/reprobado.gif');
+    }
 }
