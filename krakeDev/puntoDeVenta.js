97c82a7e260d4be5c0e29e38e20d75e1dba67fc4
Andres Villacres <andreslkf14@gmail.com>
2023-08-19 14:47:23 -0500

    Descuento ventas, funcion calcular Total ok

--- krakeDev/puntoDeVenta.js
+++ krakeDev/puntoDeVenta.js
@@ -28,7 +28,7 @@ calcularValorTotal = function(){
             Si el caso de prueba es exitoso, hacer un commit
          */
     //6. Invocar a calcularDescuento y lo que devuelve guardar en la variable valorDescuento
-    valorDescuento =  calcularValorDescuento(valorSubtotal,porcentajeDescuento)
+    valorDescuento =  calcularValorDescuento(valorSubtotal,porcentajeDescuento).toFixed(2)
     //7. Mostrar el resultado en el componente lblDescuento
     mostrarTexto('lblDescuento',valorDescuento)
         /*
@@ -39,7 +39,7 @@ calcularValorTotal = function(){
          */
     //8. Invocar a calcularIVA y lo que devuelve guardar en la variable valorIVA
 	//   El IVA debe calcularse sobre el valor del subtotal menos el descuento
-    valorIVA = calcularIva(valorSubtotal - valorDescuento)
+    valorIVA = calcularIva(valorSubtotal - valorDescuento).toFixed(2)
     //9. Mostrar el resultado en el componente lblValorIVA  
     mostrarTexto('lblValorIVA',valorIVA)  
     /*
@@ -52,7 +52,9 @@ calcularValorTotal = function(){
             Si el caso de prueba es exitoso, hacer un commit
         */
     //10. Invocar a calcularTotal y lo que devuelve guardar en la variable valorTotal
+    valorTotal = calcularTotal(valorSubtotal,valorDescuento,valorIVA).toFixed(2)
     //11. Mostrar el resultado en el componente lblTotal
+    mostrarTexto('lblTotal',valorTotal)
      /*
             Caso de prueba: 
                 precioProducto: 5.4  cantidad: 10 descuento: 10
