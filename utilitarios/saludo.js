8796e987091309e3c409adbcab66213669630005
Andres Villacres <andreslkf14@gmail.com>
2023-08-18 22:07:49 -0500

    Funciones recuperar enteros, y flotantes

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -1,6 +1,8 @@
 saludar = function(){
     let nombre = recuperarTexto('txtNombre')
     let apellido = recuperarTexto('txtApellido')
+    let edad = recuperarInt('txtEdad')
+    let estatura = recuperarFloat('txtEstatura')
 }
 recuperarTexto = function(idComponente){
     let componente;
@@ -8,4 +10,16 @@ recuperarTexto = function(idComponente){
     componente = document.getElementById(idComponente)
     valorIngresado = componente.value 
     return valorIngresado
+}
+recuperarInt = function(idComponente){
+    let valorCaja = recuperarTexto(idComponente)
+    let valorEntero = parseInt(valorCaja)
+    return valorEntero
+
+}
+recuperarFloat = function(idComponente){
+    let valorCaja = recuperarTexto(idComponente)
+    let valorFloat = parseFloat(valorCaja)
+    return valorFloat
+
 }
