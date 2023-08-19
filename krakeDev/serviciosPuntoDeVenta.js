b3e6dee20cc15878c82b4b73cbd746ab97d15641
Andres Villacres <andreslkf14@gmail.com>
2023-08-19 13:10:56 -0500

    Descuento ventas, funcion subtotal ok

---
+++ krakeDev/serviciosPuntoDeVenta.js
@@ -0,0 +1,13 @@
+calcularValorDescuento = function(monto,porcentajeDescuento){
+    return monto * porcentajeDescuento / 100
+}
+calcularIva = function(monto){
+    return monto * 12 / 100
+}
+calcularSubtotal = function(precio, cantidad){
+    return precio * cantidad
+}
+calcularTotal =function(subtotal, descuento, iva){
+    return subtotal + descuento + iva
+}
+
