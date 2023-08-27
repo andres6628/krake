a6a1aa5faf10295f994a23f485722cbe4f3917b0
Andres Villacres <andreslkf14@gmail.com>
2023-08-26 22:34:09 -0500

    funciones agregadas ok

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -6,9 +6,20 @@ calcularPromedioNotas = function(){
     let promedio = calcularPromedio(n1,n2,n3);
     promedio = promedio.toFixed(2)
     mostrarTexto('lblPromedio',promedio);
-    if (promedio > 7) {
-        mostrarImagen('imgPromedio','imagenes/200w.gif');
-    } else {
+    if (promedio > 0 && promedio < 5 ) {
+        mostrarTexto('msjProm','REPROBADO');
         mostrarImagen('imgPromedio','imagenes/reprobado.gif');
+    } else 
+    if(promedio >=5 && promedio <= 8){
+        mostrarTexto('msjProm','BUEN TRABAJO');
+        mostrarImagen('imgPromedio','imagenes/200w.gif');
+
+    }else 
+    if(promedio >8 && promedio<=10){
+        mostrarTexto('msjProm','EXCELENTE');
+        mostrarImagen('imgPromedio','imagenes/excelente.gif');
+    }else{
+        mostrarTexto('msjProm','DATOS INCORRECTOS')
+        mostrarImagen('imgPromedio','imagenes/incorrecto.gif')
     }
 }
