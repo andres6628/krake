e17a00883a4844c629f748741dbce2616525c06a
Andres Villacres <andreslkf14@gmail.com>
2023-09-05 15:57:45 -0500

    paso 1 ok

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -9,3 +9,25 @@ esMayuscula = function(caracter){
         return false;
     }
 }
+
+//PASO 1
+let palabraSecreta;
+let mayuscula = 0;
+guardarPalabra = function(){
+    let palabra = recuperarTexto('txtSecreta');
+    if (palabra.length != 5) {
+        alert('PASSWORD DEBE TENER 5 CARACTERES');
+    }
+    for (let i = 0; i < palabra.length-1; i++) {
+        let caracter = palabra.charAt(i);
+        if (esMayuscula(caracter) == false) {
+            mayuscula += 1; 
+        }
+    }
+    if (mayuscula>0) {
+           alert('LOS CARACTERES DEBEN SER MAYUSCULAS');
+    }
+    mayuscula = 0 ;
+}
+
+
